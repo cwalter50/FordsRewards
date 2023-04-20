2aab38aaa44d66307e4f2fca743da8767c487c53
Joe McNicholas <jmcnicholas90@haverfordsd.net>
2023-04-20 11:48:32 -0400

    Added a view

---
+++ FordsRewards/Joe'sView.swift
@@ -0,0 +1,20 @@
+//
+//  Joe'sView.swift
+//  FordsRewards
+//
+//  Created by Joseph McNicholas on 4/20/23.
+//
+
+import SwiftUI
+
+struct Joe_sView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Joe_sView_Previews: PreviewProvider {
+    static var previews: some View {
+        Joe_sView()
+    }
+}

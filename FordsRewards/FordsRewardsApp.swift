ac45b23e0fb54c0b89bebfc42281c7c852c4c1ee
cwalter50 <walt50@gmail.com>
2023-04-20 11:23:34 -0400

    Initial Commit

---
+++ FordsRewards/FordsRewardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FordsRewardsApp.swift
+//  FordsRewards
+//
+//  Created by Christopher Walter on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FordsRewardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

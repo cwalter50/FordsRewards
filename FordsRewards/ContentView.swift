d46a3e4cd001059f2beb0c5c98e45f6c1fa830b4
Eric <estephany80@haverfordsd.net>
2023-04-21 12:10:51 -0400

    added custom colors

--- FordsRewards/ContentView.swift
+++ FordsRewards/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundColor(.fordsLightRed)
             Text("Hello, world!")
         }
         .padding()

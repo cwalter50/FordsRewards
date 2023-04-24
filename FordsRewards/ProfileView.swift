13363cfb2817875d68e766854116fb86559a97d7
Joe McNicholas <jmcnicholas90@haverfordsd.net>
2023-04-24 11:37:45 -0400

    Profile page added

---
+++ FordsRewards/ProfileView.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileView.swift
+//  FordsRewards
+//
+//  Created by Joseph McNicholas on 4/24/23.
+//
+
+//
+//  ProfileView.swift
+//  FordsRewards
+//
+//  Created by Joseph McNicholas on 4/20/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State var displayname = ""
+    @State var bio: String = ""
+    @State var points: Int = 0
+    var body: some View {
+        NavigationView
+        {
+            VStack
+            {
+                HStack
+                {
+                    Image("HF").resizable().scaledToFit().frame(width: 100).clipShape(Circle())
+                    VStack(alignment: .center)
+                    {
+                        Text("\(displayname)").multilineTextAlignment(.center).font(.title)
+                        Text("Points: \(points)")
+                        Text("\(bio)").multilineTextAlignment(.center).italic()
+                        
+                    }
+                }
+                NavigationLink("Edit Profile")
+                {
+                    EditProfileView(displaynamereference: $displayname, bioreference: $bio)
+                }.background(.blue).bold().foregroundColor(.white)
+            }
+        }
+    }
+}
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
+
+
+
+

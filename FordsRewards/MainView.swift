4cd3240ecf88cf0052f6533bcaa279b820c7c8ac
jdec7 <joshdecter@gmail.com>
2023-04-25 12:14:17 -0400

    minor fixes to login

--- FordsRewards/MainView.swift
+++ FordsRewards/MainView.swift
@@ -27,6 +27,9 @@ struct MainView: View {
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        NavigationView{
+            MainView()
+                .environmentObject(dev.authVM)
+        }
     }
 }

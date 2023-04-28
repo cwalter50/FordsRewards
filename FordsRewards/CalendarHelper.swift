238b95d6ae5241dc19acb49eb604909320ea9780
Gavin C <gavcappelletti@gmail.com>
2023-04-28 11:54:38 -0400

    Finished Calendar View

--- FordsRewards/CalendarHelper.swift
+++ FordsRewards/CalendarHelper.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 class CalendarHelper
 {
     let calendar = Calendar.current
@@ -25,4 +26,28 @@ class CalendarHelper
     {
         return calendar.date(byAdding: .month, value: -1, to: date)!
     }
+    func daysInMonth(_ date: Date) -> Int
+    {
+        let range = calendar.range(of: .day, in: .month, for: date)!
+        return range.count
+    }
+    func dayOfMonth(_ date: Date) -> Int
+    {
+        let components = calendar.dateComponents([.day], from: date)
+        return components.day!
+    }
+    func firstOfMonth(_ date: Date) -> Date
+    {
+        let components = calendar.dateComponents([.year, .month], from: date)
+        return calendar.date(from: components)!
+    }
+    func weekDay(_ date: Date) -> Int
+    {
+
+        let components = calendar.dateComponents([.weekday], from: date)
+            return components.weekday! - 1
+    }
+    
+    
+    
 }

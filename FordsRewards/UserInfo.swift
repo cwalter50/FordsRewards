df49214354ea75b6b847ba5334a16c0c314ac8f5
Michael Potter <mpotter0905@gmail.com>
2023-10-11 13:33:04 -0400

    Add Dev Team, start docs

--- FordsRewards/UserInfo.swift
+++ FordsRewards/UserInfo.swift
@@ -17,16 +17,19 @@ class UserInfo : ObservableObject, Identifiable
     @Published var email : String
     @Published var id : String
     @Published var created : Date
+    @Published var role: String
     
-    init(firstName: String = "", lastName: String = "", points: Int = 0, email: String = "", id: String = UUID().uuidString) {
+    // Starts making the variables that are attached to each user
+    init(firstName: String = "", lastName: String = "", points: Int = 0, email: String = "", id: String = UUID().uuidString, role: String = "") {
         self.firstName = firstName
         self.lastName = lastName
         self.points = points
         self.email = email
         self.id = id
         self.created = Date()
+        self.role = role
     }
-    
+    // Checks to see if the user has all the data and if not then sets it to No _ Retrieved
     init(data:[String: Any])
     {
         self.firstName = data["firstName"] as? String ?? "No First Name Retreived"
@@ -35,9 +38,10 @@ class UserInfo : ObservableObject, Identifiable
         self.email = data["email"] as? String ?? "No Email Retreived"
         self.id = data["id"] as? String ?? "No UID Retreived"
         self.created = (data["created"] as? Timestamp)?.dateValue() ?? Date()
+        self.role = data["role"] as? String ?? "No Role Retrieved"
     }
-    
+    // Initializes the values and sets the default parameters
     func toDictionaryValues() -> [String : Any] {
-        return ["id" : self.id, "firstName" : self.firstName, "lastName" : self.lastName, "points" : self.points, "email" : self.email, "created" : self.created]
+        return ["id" : self.id, "firstName" : self.firstName, "lastName" : self.lastName, "points" : self.points, "email" : self.email, "created" : self.created, "role" : "student"]
     }
 }
